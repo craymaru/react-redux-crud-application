e80a29829ebd484e96b20d81ccdb424b7998a0d9
Cray <craymaru@gmail.com>
2020-04-23 04:37:17 +0900

    Learned state and setState.

--- src/App.js
+++ src/App.js
@@ -7,9 +7,29 @@ const App = () => (
 )
 
 class Counter extends Component {
+  
+  constructor(props) {
+    super(props)
+    console.log(this.state)
+    this.state = { count: 0 }
+  }
+
+  handleIncreaseCount = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  handleDecreaseCount = () => {
+    this.setState({ count: this.state.count - 1 })
+  }
+
   render() {
+    console.log("render")
     return (
-      <div>counter</div>
+      <React.Fragment>
+        <div>count: { this.state.count }</div>
+        <button onClick={this.handleIncreaseCount}>+1</button>
+        <button onClick={this.handleDecreaseCount}>-1</button>
+      </React.Fragment>
     )
   }
 }
