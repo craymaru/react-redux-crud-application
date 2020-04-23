a6bd7ef2032f0af957f2ae1bd30e0c8a32075b4d
Cray <craymaru@gmail.com>
2020-04-23 12:36:55 +0900

    Implement event new component

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -1,19 +1,71 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Field, reduxForm } from "redux-form";
 import { Link } from "react-router-dom";
 
-// import { postEvents } from "../actions";
+import { postEvent } from "../actions";
 
 class EventsNew extends Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  renderField(field) {
+    const {
+      input,
+      label,
+      type,
+      meta: { touched, error },
+    } = field;
+
+    return (
+      <div>
+        <input {...input} placeholder={label} type={type} />
+        {touched && error && <span>{error}</span>}
+      </div>
+    );
+  }
+
+  async onSubmit(values) {
+    await this.props.postEvent(values);
+    this.props.history.push("/");
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
       <React.Fragment>
-        <div>eventsNew</div>
+        <div>
+          <form onSubmit={handleSubmit(this.onSubmit)}>
+            <div>
+              <Field label="Title" name="title" type="text" component={this.renderField} />
+            </div>
+            <div>
+              <Field label="Body" name="body" type="text" component={this.renderField} />
+            </div>
+
+            <div>
+              <input type="submit" value="Submit" disabled={false} />
+              <Link to="/"> Back </Link>
+            </div>
+          </form>
+        </div>
       </React.Fragment>
     );
   }
 }
 
-// const mapDispatchToProps = ({ postEvents });
+const validate = (values) => {
+  const errors = {};
 
-export default connect(null, null)(EventsNew);
+  if (!values.title) errors.title = "Please enter a title.";
+  if (!values.body) errors.body = "Please enter a body.";
+
+  return errors;
+};
+
+const mapDispatchToProps = { postEvent };
+
+export default connect(null, mapDispatchToProps)(reduxForm({ validate, form: "eventNewForm" })(EventsNew));
